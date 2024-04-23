778e8bf61c0e83ea69668082cfe65ea151c41149
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-04-23 15:45:29 +0800

    Update init.lua

--- lua/nvimplug/init.lua
+++ lua/nvimplug/init.lua
@@ -1,1 +1,20 @@
+local M = {}
 
+M.opts = {
+    plug_config_dir = vim.fs.normalize(vim.fn.stdpath('config') .. '/lua/plugins'),
+    plug_config_edit_cmd = 'edit',
+    plug_config_loadall = true,
+}
+
+M.setup = function(plugins, opts)
+    require('nvimplug').plug_regist(plugins)
+
+    M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+
+    if M.opts.plug_config_loadall then
+	M.loadallconfig()
+    end
+
+end
+
+return M
