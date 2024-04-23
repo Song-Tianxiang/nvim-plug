1a69370a8ddf966125c20d2a4979958b2cd01f82
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-04-23 17:49:10 +0800

    Update boot_vimplug.lua

--- lua/nvim-plug/boot_vimplug.lua
+++ lua/nvim-plug/boot_vimplug.lua
@@ -2,5 +2,6 @@ local vimplugfile = vim.fn.stdpath('data') .. '/site/autoload/plug.vim'
 local vimplugurl = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 if vim.fn.filereadable(vimplugfile) == 0 then
 	vim.fn.system(string.format('curl -fsSLo %s --create-dirs %s', vimplugfile, vimplugurl))
+	vim.cmd('source ' .. vimplugfile)
 	vim.cmd[[ autocmd VimEnter * PlugInstall --sync | source $MYVIMRC ]]
 end
