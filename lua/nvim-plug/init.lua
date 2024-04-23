d764f6e1a431184427ef7cd82e60e76f9c92db6a
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-04-23 17:43:13 +0800

    Update init.lua

--- lua/nvim-plug/init.lua
+++ lua/nvim-plug/init.lua
@@ -8,13 +8,14 @@ M.opts = {
 }
 
 M.setup = function(plugins, opts)
+    M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+	
     require('nvim-plug.boot_vimplug')
 
     local plugins = plugins or {}
-    table.insert(plugins, 1, { 'Song-Tianxiang/nvim-plug' } )
-    require('nvim-plug.plug_register').plug_register(plugins)
-
-    M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+    table.insert(plugins, 1, { 'Song-Tianxiang/nvim-plug' dir = vim.fs.normalize(vim.fn.stdpath('data') .. '/nvim-plug/nvim-plug') } )
+    table.insert(plugins, 2, { 'junegunn/vim-plug' } )
+    require('nvim-plug.plug_register').plug_register(plugins, plug_config_plug_home)
 
     if M.opts.plug_config_loadall then
 	require('nvim-plug.load_config').load_all_config()
