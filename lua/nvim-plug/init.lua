6aab06ee4d02707af37c2808ff6fc00050f5bdc5
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-04-23 19:06:51 +0800

    Update init.lua

--- lua/nvim-plug/init.lua
+++ lua/nvim-plug/init.lua
@@ -18,7 +18,7 @@ M.setup = function(plugins, opts)
     require('nvim-plug.plug_register').plug_register(plugins, plug_config_plug_home)
 	if vim.g.plug_config_bootstrap then
 		vim.cmd[[ PlugInstall --sync ]]
-		require('nvim-plug.plug_register').plug_register(plugins, plug_config_plug_home) ]]
+		require('nvim-plug.plug_register').plug_register(plugins, plug_config_plug_home)
 	end
 
     if M.opts.plug_config_loadall then
